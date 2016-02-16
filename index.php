f08d01e1ad2065eec3fd7c1e5b26451c1d616155
Sam Davis <sam@sjd.co>
2016-02-17 10:06:19 +1100

    better filepath handling

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: Forge Customisations
+Plugin Name: Forge Tweaks
 Description: Our lovingly-made WP Customisation Plugin.
 Author: Forge Collective
 Version: 1.0
@@ -16,8 +16,8 @@ Network: True
 */
 function forge_dashboard_footer()
 {
-	if (is_admin()) {
-		wp_enqueue_style( 'dashboard', plugins_url( 'forge-customisations/css/dashboard.css' ), array(), '1.0' );
+	if ( is_admin() ) {
+		wp_enqueue_style( 'dashboard', plugin_dir_url( __FILE__ ) . 'css/dashboard.css', array(), '1.0' );
 		wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
 	}
 	echo 'Made with <i class="fa fa-heart"></i> by <a href="http://weareforge.co" target="_blank">Forge</a>.';
@@ -32,7 +32,7 @@ add_filter('admin_footer_text', 'forge_dashboard_footer');
 */
 
 function forge_login_css() {
-    echo '<link rel="stylesheet" type="text/css" href="' . plugins_url( 'forge-customisations/css/wp-login.css' ) . '">';
+    echo '<link rel="stylesheet" type="text/css" href="' . plugin_dir_url( __FILE__ ) . 'css/wp-login.css">';
 }
 
 add_action('login_head', 'forge_login_css');
