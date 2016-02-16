702c94b9fbe7ad36edab7bdbe95649424569f8b3
Sam Davis <sam@sjd.co>
2016-02-16 12:38:38 +1100

    update paths

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ Network: True
 function forge_dashboard_footer()
 {
 	if (is_admin()) {
-		wp_enqueue_style( 'dashboard', plugins_url( 'forge-wp/css/dashboard.css' ), array(), '1.0' );
+		wp_enqueue_style( 'dashboard', plugins_url( 'forge-customisations/css/dashboard.css' ), array(), '1.0' );
 		wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
 	}
 	echo 'Made with <i class="fa fa-heart"></i> by <a href="http://weareforge.co" target="_blank">Forge</a>.';
@@ -32,7 +32,7 @@ add_filter('admin_footer_text', 'forge_dashboard_footer');
 */
 
 function forge_login_css() {
-    echo '<link rel="stylesheet" type="text/css" href="' . plugins_url( 'forge-wp/css/wp-login.css' ) . '">';
+    echo '<link rel="stylesheet" type="text/css" href="' . plugins_url( 'forge-customisations/css/wp-login.css' ) . '">';
 }
 
 add_action('login_head', 'forge_login_css');
