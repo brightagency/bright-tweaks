a95a24c84c41dab50113378b2c9a00ed77a66182
Sam Davis <sam@sjd.co>
2016-06-17 10:47:55 +1000

    Object orientify

---
+++ forge-tweaks.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://bitbucket.org/forgecollective/forge-tweaks
+ * @since             2.0.0
+ * @package           Forge_Tweaks
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Forge Tweaks
+ * Plugin URI:        https://bitbucket.org/forgecollective/forge-tweaks
+ * Description:       Our lovingly-made WP Customisation Plugin.
+ * Version:           2.0.0
+ * Author:            Forge Collective
+ * Author URI:        http://frge.co
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-forge-tweaks.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    2.0.0
+ */
+function run_forge_tweaks() {
+
+	$forge_tweaks = new Forge_Tweaks();
+	$forge_tweaks->run();
+
+}
+run_forge_tweaks();
---
+++ includes/class-forge-tweaks.php
@@ -0,0 +1,111 @@
+<?php
+
+class Forge_Tweaks {
+
+    /**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+    protected $plugin_name;
+
+    /**
+	 * The current version of the plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+    protected $version;
+
+    /**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    2.0.0
+	 */
+    public function __construct() {
+
+        $this->plugin_name = "Forge Tweaks";
+        $this->version = "2.0.0";
+
+        $this->define_admin_hooks();
+        $this->define_public_hooks();
+
+    }
+
+    /**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 */
+    private function define_admin_hooks() {
+
+        function forge_dashboard_footer()
+        {
+
+        	if ( is_admin() ) {
+        		wp_enqueue_style( 'dashboard-css', plugin_dir_url( __FILE__ ) . '../assets/css/dashboard.css' );
+        	}
+        	echo 'Made with <i class="fa fa-heart"></i> by <a href="http://weareforge.co" target="_blank">Forge</a>.';
+        }
+
+        add_filter('admin_footer_text', 'forge_dashboard_footer');
+
+    }
+
+    /**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 */
+    private function define_public_hooks() {
+
+        function forge_login_css() {
+            echo '<link rel="stylesheet" type="text/css" href="' . plugin_dir_url( __FILE__ ) . '../assets/css/wp-login.css">';
+        }
+
+        add_action('login_head', 'forge_login_css');
+
+    }
+
+    /**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    2.0.0
+	 */
+    public function run() {
+
+    }
+
+    /**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     2.0.0
+	 * @return    string    The name of the plugin.
+	 */
+    public function get_plugin_name() {
+        return $this->plugin_name;
+    }
+
+    /**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     2.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+    public function get_version() {
+        return $this->version;
+    }
+
+}
